861ea69db13eef05dc5d17afad5321531a0f97af
Khaydanov Yuriy <UnLess24@users.noreply.github.com>
2017-10-25 13:42:34 +0300

    Add files via upload

---
+++ delivery.php
@@ -0,0 +1,66 @@
+<?php 
+class Delivery
+{
+  private $deliveryValue;
+  private $everyKgPrice;
+  private $periodLimit = NULL;
+  private $firstPeriodPrice = NULL;
+  private $secondPeriodPrice = NULL;
+  
+  public function __construct($deliveryValue = 0, $everyKgPrice = 0, $periodLimit = NULL, $firstPeriodPrice = NULL, $secondPeriodPrice = NULL)
+  {
+    $this->deliveryValue = (is_numeric($deliveryValue) && $deliveryValue >= 0) ? $deliveryValue : 0;
+    $this->everyKgPrice = (is_numeric($everyKgPrice) && $everyKgPrice >= 0) ? $everyKgPrice : 0;
+    
+    if ($everyKgPrice == 0 && isset($options)) {
+      $this->periodLimit = $periodLimit;
+      $this->firstPeriodPrice = $firstPeriodPrice;
+      $this->secondPeriodPrice = $secondPeriodPrice;
+    }
+  }
+
+  // Если цена до лимита ЗА КИЛОГРАММ, после лимита ИТОГО
+  public function getPrice()
+  {
+    if ($this->isPeriodPrice()) {
+      if ($this->deliveryValue <= $this->periodLimit) {
+        return $this->deliveryValue * $this->firstPeriodPrice; 
+      } else {
+        return $this->secondPeriodPrice;
+      }
+    }
+    return $this->deliveryValue * $this->everyKgPrice;
+  }
+
+  // Если цена до лимита и после - ЗА КИЛОГРАММ
+  public function getPriceLimitKG()
+  {
+    if ($this->isPeriodPrice()) {
+      if ($this->deliveryValue <= $this->periodLimit) {
+        return $this->deliveryValue * $this->firstPeriodPrice; 
+      } else {
+        return $this->deliveryValue * $this->secondPeriodPrice;
+      }
+    }
+    return $this->deliveryValue * $this->everyKgPrice;
+  }
+
+  // Если цена до лимита и после - ИТОГО
+  public function getPriceLimit()
+  {
+    if ($this->isPeriodPrice()) {
+      if ($this->deliveryValue <= $this->periodLimit) {
+        return $this->firstPeriodPrice; 
+      } else {
+        return $this->secondPeriodPrice;
+      }
+    }
+    return $this->deliveryValue * $this->everyKgPrice;
+  }
+
+  private function isPeriodPrice()
+  {
+    return !is_null($this->periodLimit);
+  }
+}
+?>
